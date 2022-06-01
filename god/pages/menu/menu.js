07b089fbcc0da58231ff604b0f71fe562f6f0f0a
Juan Rueda <97234015+ByJuanCho98@users.noreply.github.com>
2022-06-01 10:24:27 -0500

    Delete god directory

--- god/pages/menu/menu.js
+++
@@ -1,7 +0,0 @@
-const splash = document.querySelector('.splash');
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    setTimeout(() => {
-        splash.classList.add('display-none');
-    }, 1200);
-})
